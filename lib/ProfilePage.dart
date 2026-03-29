29a61919a2b304338e80993a12e0d7833df36527
rintymoni01 <rintymoni01@gmail.com>
2026-03-29 20:48:10 +0600

    feat: add ProfilePage UI and configure image assets in pubspec

--- lib/ProfilePage.dart
+++ lib/ProfilePage.dart
@@ -12,43 +12,78 @@ class Profilepage extends StatefulWidget {
 class _ProfilepageState extends State<Profilepage> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
-        leading: IconButton(onPressed: (){
-          Get.back();
-        }, icon: Icon(Icons.arrow_back_outlined, size: 16,color: Colors.blueAccent,)),
+        leading: IconButton(
+          onPressed: () {
+            Get.back();
+          },
+          icon: Icon(
+            Icons.arrow_back_outlined,
+            size: 16,
+            color: Colors.blueAccent,
+          ),
+        ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: Column(
-
           children: [
             Row(
               children: [
-                Icon(Icons.home , size: 16,),
-                SizedBox(width: 170,),
-                Text("Hlw Rinty",style: TextStyle(color: Colors.deepPurple, fontSize: 20 ,fontWeight: FontWeight.w300),),
-                SizedBox(width: 170,),
-                Icon(Icons.search)
-
+                Icon(Icons.home, size: 16),
+                SizedBox(width: 170),
+                Text(
+                  "Hlw Rinty",
+                  style: TextStyle(
+                    color: Colors.deepPurple,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+                SizedBox(width: 170),
+                Icon(Icons.search),
               ],
             ),
-            SizedBox(height: 16,),
+            SizedBox(height: 16),
             Container(
-              height: 30,
+              height: 40,
               width: 300,
-              decoration: BoxDecoration(color: Colors.grey, borderRadius: BorderRadius.circular(18)),
+              decoration: BoxDecoration(
+                color: Colors.black12,
+                borderRadius: BorderRadius.circular(18),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(Icons.location_on, color: Colors.lightBlueAccent),
+                    Text("hlw mr.Sakil Vaiya. Keya hal hey"),
+
+                    Icon(Icons.facebook, color: Colors.indigo),
+                  ],
+                ),
+              ),
             ),
-            SizedBox(height: 10,),
+            SizedBox(height: 10),
             Container(
               height: 100,
               width: 100,
-              decoration: BoxDecoration(color: Colors.cyanAccent, borderRadius: BorderRadius.circular(50)),
-            )
-            
+              decoration: BoxDecoration(
+                color: Colors.cyanAccent,
+                borderRadius: BorderRadius.circular(50),
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(50),
+                child: Image.asset(
+                  'assets/Images/rinty.jpg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
           ],
         ),
-
       ),
     );
   }
