269a36efdd9432b3282012cc828d53e54aa58494
rintymoni01 <rintymoni01@gmail.com>
2026-03-30 20:02:51 +0600

    feat: add Myproject screen with browser-style header and profile card layout

--- lib/MyProject.dart
+++ lib/MyProject.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get_x/get_core/src/get_main.dart';
+import 'package:get_x/get_navigation/src/extension_navigation.dart';
 
 class Myproject extends StatefulWidget {
   const Myproject({super.key});
@@ -59,21 +61,137 @@ class _MyprojectState extends State<Myproject> {
             ),
             Padding(
               padding: const EdgeInsets.all(12),
-              child: Container(
-                width: 300,
-                height: 304,
-                decoration: BoxDecoration(
-                  border: Border.all(width: 2, color: Colors.blue),
-                ),
-                child: Column(
-                  children: [
-                    Image.asset(
-                      "assets/Images/rinty.jpg",
-                      width: double.infinity,
-                      height: 152,
-                      fit: BoxFit.cover,
-                    ),
-                  ],
+              child: GestureDetector(
+                onTap: (){
+                  Get.to(()=> )
+                },
+                child: Container(
+                  width: 300,
+                  height:324,
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 2, color: Colors.blue),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Image.asset(
+                        "assets/Images/rinty.jpg",
+                        width: double.infinity,
+                        height: 152,
+                        fit: BoxFit.cover,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(16),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: [
+                                Text(
+                                  "Name :",
+                                  style: TextStyle(
+                                    color: Colors.deepPurple,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(width: 8),
+                                Text(
+                                  "Rinty",
+                                  style: TextStyle(
+                                    color: Colors.blue,
+                                    fontWeight: FontWeight.w400,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ],
+                            ),
+
+                            Row(
+                              children: [
+                                Text(
+                                  "Seation :",
+                                  style: TextStyle(
+                                    color: Colors.deepPurple,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(width: 8),
+                                Text(
+                                  "21/22",
+                                  style: TextStyle(
+                                    color: Colors.blue,
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+
+                            Row(
+                              children: [
+                                Text(
+                                  "Depertment :",
+                                  style: TextStyle(
+                                    color: Colors.deepPurple,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(width: 8),
+                                Text(
+                                  "CST",
+                                  style: TextStyle(
+                                    color: Colors.blue,
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Text(
+                                  "Bio :",
+                                  style: TextStyle(
+                                    color: Colors.deepPurple,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 8,
+                                ),
+                                Text(
+                                  "I am a Rinty.",
+                                  style: TextStyle(
+                                    color: Colors.blue,
+                                    fontWeight: FontWeight.w400,
+                                    fontSize: 14,
+                                  ),
+                                ),
+
+                              ],
+                            ),
+
+
+                            Padding(
+                              padding:  EdgeInsets.only(left: 40),
+                              child: Text(
+                                " I have a Apps Developer.",
+                                style: TextStyle(
+                                  color: Colors.lightBlueAccent,
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
