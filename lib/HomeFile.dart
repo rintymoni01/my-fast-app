ab215ca6915d5bf25b19bbf620b2e8e96abeb65c
rintymoni01 <rintymoni01@gmail.com>
2026-04-04 20:44:27 +0600

    feat: create Homefile widget with search bar and header UI components

--- lib/HomeFile.dart
+++ lib/HomeFile.dart
@@ -13,12 +13,15 @@ class _HomefileState extends State<Homefile> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.all(20),
+        padding: const EdgeInsets.all(15),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Row( mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Column(
+
                   children: [
                     Text(
                       "AYA Shop",
@@ -28,11 +31,11 @@ class _HomefileState extends State<Homefile> {
                         fontWeight: FontWeight.w700,
                       ),
                     ),
-                    SizedBox(height: 4,),
+                    SizedBox(height: 4),
                     Row(
                       children: [
                         Image.asset("assets/Images/Location.png"),
-                        SizedBox(width: 4,),
+                        SizedBox(width: 4),
                         Text(
                           "Barcelona, Spain",
                           style: TextStyle(
@@ -45,10 +48,53 @@ class _HomefileState extends State<Homefile> {
                     ),
                   ],
                 ),
-                Image.asset("assets/Images/Notification.png")
+                Padding(
+                  padding: const EdgeInsets.only(right: 18),
+                  child: Image.asset("assets/Images/Notification.png"),
+                ),
               ],
-
             ),
+            SizedBox(height: 10),
+            Row(
+              children: [
+                Container(
+                  height: 50,
+                  width: 400,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(color: Color(0xFF0A5C36)),
+                  ),
+                  child: Row(
+                    children: [
+                      SizedBox(width: 12),
+                      Image.asset("assets/Images/Search.png"),
+                      SizedBox(width: 15),
+                      Text(
+                        "Search  services...",
+                        style: TextStyle(
+                          color: Color(0xff79797980),
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(width: 15,),
+                Container(
+                  height: 50,
+                  width: 48,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(
+                        color: Color(0xff0A5C36),
+                      ),
+                  ),
+                  child:
+                   Image.asset("assets/Images/container.png"),
+                ),
+              ],
+            )
           ],
         ),
       ),
