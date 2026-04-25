a967a15af25f0bc4d933bc49a04f55691bd99ee0
rintymoni01 <rintymoni01@gmail.com>
2026-04-25 20:47:52 +0600

    feat: add familyMedicine view scaffold

---
+++ lib/services/View/family-Medicine.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class familyMedicine extends StatelessWidget {
+  const familyMedicine({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:
+      Column(
+        children: [
+
+        ],
+      )
+      ,
+    );
+  }
+}
+
